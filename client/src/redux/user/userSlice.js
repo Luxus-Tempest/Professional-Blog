8156419fa0f2ae720fd914487112ae7d5a801a6d
Luxus-Tempest <bahibmail@gmail.com>
2024-02-14 15:59:21 +0100

    add redux persist

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,30 +1,30 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser: null,
-    error: null,
-    loading: false
-}
+  currentUser: null,
+  error: null,
+  loading: false,
+};
 
-const userSlice = createSlice ({
-    name: 'user',
-    initialState,
-    reducers: {
-        signInStart: (state) => {
-            state.loading = true;
-            state.error = null;
-        },
-        signInSuccess: (state, action) => {
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-        },
-        signInFailure: (state, action) => {
-            state.loading = false;
-            state.error = action.payload;
-        }
-    }
-})
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
 
 export const { signInFailure, signInSuccess, signInStart } = userSlice.actions;
 
